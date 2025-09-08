5d9b57720ffff04cbd915e0711b0af927c009517
TuckerQuinnUNO <tquinn9@unomaha.edu>
2025-09-08 01:25:31 +0000

    Completed Assignment

--- FutureTime.py
+++ FutureTime.py
@@ -9,21 +9,27 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  moreHours = int(input("How many hours?"))
   #Ask user for minutes
-
+  moreMins = int(input("How many minutes?"))
   #Calculate the time after the user-supplied time has passed.
+  extraHour = (currentMinute + moreMins) // 60 
+  futureMins = (currentMinute + moreMins) % 60
+  futureHours = (currentHour + moreHours + extraHour) % 24
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+ 
+  print(futureHours,":",futureMins)
 
 if __name__ == '__main__':
   main()
+ 
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Tucker Quinn
+#Date: September 7th, 2025
+#Assignment: Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,16 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  input("What is your question?")
   #Answer question randomly with one of the options from your earlier list.
-
+  answers = ["As I see it, yes" , "Ask again later." , "Better not tell you now" , "Cannot predict now." , "Concentrate and ask again." , "Don't count on it." , 
+            "It is certain." , "It is decidedly so." , "Most likely." , "My reply is no." , "My sources say no." , "Outlook not so good." , "Outlook good." , 
+            "Reply hazy, try again." , "Signs point to yes." , "Very doubtful." , "Without a doubt." , "Yes." , "Yes - definitely." , "You may rely on it."]  # list of questions
+  length=len(answers) 
+  r = random.random() * length 
+  r = int(r) # removes decimals 
+  response = answers[r]
+  print(response)
 
 if __name__ == '__main__':
   main()
